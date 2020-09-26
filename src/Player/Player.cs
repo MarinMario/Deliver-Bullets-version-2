f73951dca163e087f4e48fa73957331d27ac2ffd
Marin Mario <38869813+marinmario@users.noreply.github.com>
2020-09-26 20:45:51 +0300

    feat: played idle and walk animations

--- src/Player/Player.cs
+++ src/Player/Player.cs
@@ -9,24 +9,43 @@ public class Player : Entity
     int acceleration = 500;
     [Export]
     int friction = 1000;
+
+    enum AnimationState { Idle, Walk }
+
+    AnimationState animationState = AnimationState.Idle;
     Vector2 velocity;
 
     protected override void Process(float delta)
     {
-        //player movement
-        Vector2 motionVector;
+        var motionVector = Vector2.Zero;
+        var sprites = GetNode<Node2D>("Sprites");
+
         motionVector.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
         motionVector.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
 
         if (motionVector != Vector2.Zero)
+        {
             velocity = velocity.MoveToward(motionVector * maxSpeed, acceleration * delta);
-        else 
+            animationState = AnimationState.Walk;
+
+            //I have to also check with an else if so sprites.Scale won't be set to (-1, 1) when the player moves up or down only
+            if (motionVector.x > 0)
+                sprites.Scale = new Vector2(1, 1);
+            else if (motionVector.x < 0)
+                sprites.Scale = new Vector2(-1, 1);
+        }
+        else
+        {
             velocity = velocity.MoveToward(Vector2.Zero, friction * delta);
+            animationState = AnimationState.Idle;
+        }
 
-        velocity = MoveAndSlide(velocity);
 
-        //shoot
+        MoveAndSlide(velocity);
+        sprites.GetNode<AnimatedSprite>("AnimatedSprite").Play(animationState.ToString());
+
         if (Input.IsActionJustPressed("shoot")) Shoot();
+
     }
 
     private void Shoot()
